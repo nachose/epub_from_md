1f92582a07dabb78ad638e050459c09888b0e3b0
Jose Ignacio Seco <joseignacio.secosanz@amadeus.com>
2023-12-06 11:36:22 +0100

    Changes

--- epub_creator.py
+++ epub_creator.py
@@ -1,58 +1,82 @@
 import os
 import re
 import sys
-import pypandoc
 
-def convert_markdown_to_epub(input_file, output_file):
+# @brief convert_markdown_to_epub 
+#        Convert input file to just one epub. 
+#
+# @param input_file      [in]    Input file. Beware the function is recursive
+# @param output_file     [in]    Output file, as the end goal is to have just
+#                                one output file.
+# @param recursion_depth [in]    recursion depth, for now just for debugging.
+# @param added_links     [inout] links already added. This is because markdonw files,
+#                                being unstructured, can have circular references, as is the case
+#                                in which I'm working now.
+#
+# @return Exit code.
+def convert_markdown_to_epub(input_file, output_file, recursion_depth, added_links):
     # Read the Markdown file
+    # print("Read markdown file: " + input_file)
     with open(input_file, 'r') as f:
         markdown_content = f.read()
 
     # Find links and print them.
-    find_links(markdown_content)
-
-    # Extract links to other Markdown files
-    links = extract_links(markdown_content)
+    links = find_links(markdown_content)
 
     # Convert linked Markdown files recursively
     for link in links:
-        linked_file = os.path.join(os.path.dirname(input_file), link)
+        # print (link)
+        linked_file = os.path.join(os.path.dirname(input_file), str(link))
+        print ("linked_file " + linked_file)
+
+        already_added = added_links.count(linked_file) > 0
+        if already_added:
+            print ("already added: " + linked_file)
+            continue
+        added_links.append(linked_file)
         if os.path.isfile(linked_file):
-            convert_markdown_to_epub(linked_file, output_file)
+            # print("Call recursively to convert_markdown_to_epub")
+            convert_markdown_to_epub(linked_file,
+                                     output_file,
+                                     recursion_depth + 1,
+                                     added_links)
+
+    output_filename = os.path.basename(input_file) + ".epub"
+    output_filename.replace(" ", "_")
 
     # Convert the current Markdown file to EPUB
-    pypandoc.convert_file(input_file, 'epub', outputfile=output_file)
-
-def extract_links(markdown_content):
-    # Regular expression to match Markdown links
-    link_regex = r"\[(?P<link>.+?)\]\((?P<url>.+)\)"
-
-    # Extract links from the Markdown content
-    links = []
-    for match in re.finditer(link_regex, markdown_content):
-        # link = match.group('link')
-        url = match.group('url')
-
-        # Check if the link is a relative path to a Markdown file
-        if url.startswith('./') or url.startswith('../'):
-            links.append(url[2:])
-
-    print (links)
-    return links
+    # os.system("pandoc " + input_file + " -o " + output_filename)
+    tabs_init = ''
+    while(recursion_depth):
+        tabs_init = tabs_init + "  "
+        recursion_depth = recursion_depth -1
+    print(tabs_init + input_file + " to file " + output_filename)
 
 def find_links(markdown_content):
-    link_pattern = r'\[([^\]]+)\]\((http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)\)'
+    link_pattern = r'\[([^\]]+)\]\(([^)]+)\)'
     links = re.findall(link_pattern, markdown_content)
 
-    for link in links:
-        print(f"Link text: {link[0]}")
-        print(f"URL: {link[1]}")
+    # for link in links:
+    #     print(f"Link text: {link[0]}")
+    #     print(f"URL: {link[1]}")
+
+    files = [link[1] for link in links]
+
+    return files
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print ("Usage python3 epub_creator.py entrypoint_file.md")
+        exit (1)
     entrypoint_file = sys.argv[1]
     # entrypoint_file = 'documentation.md'  # Replace with the actual filename
-    output_file = 'output.epub'
+    output_file = entrypoint_file + ".epub"
 
-    convert_markdown_to_epub(entrypoint_file, output_file)
+    added_links = []
+
+    convert_markdown_to_epub(entrypoint_file, 
+                             output_file, 
+                             1,
+                             added_links)
 
