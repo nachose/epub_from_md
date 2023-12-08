09766bcbd1b2fe876c2e4336ed73b2080ee37dd5
Jose Ignacio Seco <joseignacio.secosanz@amadeus.com>
2023-12-08 10:03:22 +0100

    More changes. Add TODO file

--- epub_creator.py
+++ epub_creator.py
@@ -2,23 +2,34 @@ import os
 import re
 import sys
 
-# @brief convert_markdown_to_epub 
-#        Convert input file to just one epub. 
+# @brief convert_markdown_to_epub
+#        Convert input file to just one epub.
 #
 # @param input_file      [in]    Input file. Beware the function is recursive
-# @param output_file     [in]    Output file, as the end goal is to have just
-#                                one output file.
 # @param recursion_depth [in]    recursion depth, for now just for debugging.
 # @param added_links     [inout] links already added. This is because markdonw files,
 #                                being unstructured, can have circular references, as is the case
 #                                in which I'm working now.
-# @param contents        [inout] Contents of the markdown file.
+# @param contents        [in]    Contents of the markdown file. I learnt today that string is immutable in python.
 #
 # @return Exit code.
 def convert_markdown_to_epub(input_file,
                              recursion_depth,
                              added_links,
                              contents):
+
+    contents = iterate_files(input_file, recursion_depth, added_links, contents)
+
+    output_filename = get_output_filename(input_file)
+
+    print_recursion(recursion_depth, input_file, output_filename)
+
+
+    # Convert the current Markdown file to EPUB
+    if recursion_depth == 1:
+        write_files(contents, output_filename)
+
+def iterate_files(input_file, recursion_depth, added_links, contents):
     # Read the Markdown file
     # print("Read markdown file: " + input_file)
     with open(input_file, 'r') as f:
@@ -38,24 +49,16 @@ def convert_markdown_to_epub(input_file,
 
         already_added = added_links.count(linked_file) > 0
         if already_added:
-            # print ("already added: " + linked_file)
+            print ("already added: " + linked_file)
             continue
         added_links.append(linked_file)
         if os.path.isfile(linked_file):
             # print("Call recursively to convert_markdown_to_epub")
-            convert_markdown_to_epub(linked_file,
+            contents = iterate_files(linked_file,
                                      recursion_depth + 1,
                                      added_links,
                                      contents)
-
-    output_filename = get_output_filename(input_file)
-
-    print_recursion(recursion_depth, input_file, output_filename)
-
-
-    # Convert the current Markdown file to EPUB
-    if recursion_depth == 1:
-        write_files(contents, output_filename)
+    return contents
 
 def print_recursion(recursion_depth, input_file, output_filename):
 
@@ -63,7 +66,7 @@ def print_recursion(recursion_depth, input_file, output_filename):
     while(recursion_depth):
         tabs_init = tabs_init + "  "
         recursion_depth = recursion_depth -1
-    print(tabs_init + input_file + " to file " + output_filename)
+    # print(tabs_init + input_file + " to file " + output_filename)
 
 
 def get_output_filename(input_file):
@@ -73,7 +76,11 @@ def get_output_filename(input_file):
     return output_filename
 
 def write_files(content, output_file):
-    print("Write also markdown to file")
+
+    #Replace images for it's true path.
+    process_images(content)
+
+    print("Write also markdown to file: result_markdown.md")
     g = open("result_markdown.md", "wt")
     g.write(content)
     g.close()
@@ -81,6 +88,11 @@ def write_files(content, output_file):
 
     print("Finally creating the file:")
     os.system("pandoc " + "result_markdown.md" + " -o " + output_file)
+
+# Substitute routes for images. Taking into account that we are in the current directory, it must
+# search for the images in the subdirectories, then use the correct path.
+def process_images(content):
+    find_images(content)
 
 
 def find_links(markdown_content):
@@ -101,6 +113,15 @@ def find_links(markdown_content):
         files.append(link[1])
 
     return files
+
+def find_images(markdown_content):
+
+    img_pattern = r'!\[.*\]\((.*\.jpg|.*\.jpeg|.*\.png)\)'
+    images = re.findall(img_pattern, markdown_content)
+
+    for img in images:
+        print("Img found : " + img)
+
 
 
 if __name__ == '__main__':
